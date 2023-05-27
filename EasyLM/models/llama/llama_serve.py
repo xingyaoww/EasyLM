pad to fix size for loglikelihood

EasyLM/models/llama/llama_serve.py
@@ -176,7 +176,7 @@ def main(argv):
                 # truncation=True,
                 # max_length=FLAGS.input_length,
                 return_tensors='np',
-                pad_to_multiple_of=FLAGS.pad_to_multiple_of,
+                # pad_to_multiple_of=FLAGS.pad_to_multiple_of,
             )
             input_length = prefix.input_ids.shape[1]
             inputs = tokenizer(
@@ -186,7 +186,7 @@ def main(argv):
                 max_length=FLAGS.seq_length - input_length,
                 # pad the entire sequence to max_length for likeihood calculation
                 return_tensors='np',
-                pad_to_multiple_of=FLAGS.pad_to_multiple_of,
+                # pad_to_multiple_of=FLAGS.pad_to_multiple_of,
             )
             output_tokens = np.concatenate([prefix.input_ids, inputs.input_ids], axis=1)
             bos_tokens = np.full(